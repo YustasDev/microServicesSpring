7d09f16aebc2a390327f6b63ca2cefe8fa89b17b
vitaly.gusev <vitaly.gusev@progforce.com>
2024-03-14 12:38:54 +0300

    + LicenseController() v1

--- src/main/java/goosseff/site/microservicesspring/controller/LicenseController.java
+++ src/main/java/goosseff/site/microservicesspring/controller/LicenseController.java
@@ -1,12 +1,48 @@
 package goosseff.site.microservicesspring.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import goosseff.site.microservicesspring.model.License;
+import goosseff.site.microservicesspring.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="v1/organization/{organizationId}/license")
 public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+
+    @GetMapping(value="/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+                                              @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId,organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
+                                                @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+
 
 
 
---
+++ src/main/java/goosseff/site/microservicesspring/model/License.java
@@ -0,0 +1,15 @@
+package goosseff.site.microservicesspring.model;
+
+
+import lombok.Data;
+
+@Data
+public class License {
+    private int id;
+    private String licenseId;
+    private String description;
+    private String organizationId;
+    private String productName;
+    private  String licenseType;
+
+}
